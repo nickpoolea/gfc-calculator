1e286851aff0ae8c0e997f5ef3ba25ce1e8ba388
Nick Poole <NickPooleA@gmail.com>
2017-08-11 13:45:35 -0700

    Added initial calculator logic

---
+++ src/main/java/com/example/demo/controllers/calculatorController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class calculatorController {
+	
+	public int intOne;
+	public int intTwo;
+	
+	public calculatorController() {
+		
+	}
+	
+	@GetMapping("")
+	public String index() {
+		return "/index";
+	}
+	
+	@PostMapping("calc")
+	public ModelAndView calcHandler(String selection, 
+									@RequestParam("left")  double intOne, 
+									@RequestParam("right") double intTwo) {
+		
+		ModelAndView mv = new ModelAndView("/results");
+		double result = 0;
+		
+		switch(selection) {
+		
+		case "add"     : result = intOne + intTwo;
+		break;
+		case "subtract": result = intOne - intTwo;
+		break;
+		case "multiply": result = intOne * intTwo;
+		break;
+		case "divide"  : result = intOne / intTwo;
+		break;
+		case "modular" : result = intOne % intTwo;
+		break;
+		case "exponent": result = Math.pow(intOne,  intTwo);
+		break;
+		
+		}
+		
+		mv.addObject("result", result);
+		return mv;
+		
+	}
+
+}
